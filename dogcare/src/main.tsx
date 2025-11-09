ce60be85d777cc70bf5d6043258e81129cc26582
VadimGRSM <166593196+VadimGRSM@users.noreply.github.com>
2025-11-09 16:59:41 +0200

    Simplify UI layer and fix theming

--- dogcare/src/main.tsx
+++ dogcare/src/main.tsx
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./styles/globals.css"
 import { AppThemeProvider } from "./providers/theme"
-import { Toaster } from "sonner"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AppThemeProvider>
       <App />
-      <Toaster richColors />
     </AppThemeProvider>
   </React.StrictMode>
 )
--- dogcare/src/providers/theme.tsx
+++ dogcare/src/providers/theme.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from "next-themes"
-import { ReactNode } from "react"
+import type { ReactNode } from "react"
 
 export function AppThemeProvider({ children }: { children: ReactNode }) {
   return (
